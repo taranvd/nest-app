5e5f2818ab61f819806239e8238a64dc4be1cc53
taranvd <apple.vlad.taran@gmail.com>
2024-01-16 19:12:15 +0200

    feat: added guards

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Post, Body, Get } from '@nestjs/common'
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common'
 import { CreateUserDto } from './dto/create-user-dto'
 import { UsersService } from './users.service'
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
 import { User } from './user.model'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 
 @ApiTags('Users')
 @Controller('users')
@@ -18,6 +19,7 @@ export class UsersController {
 
   @ApiOperation({ summary: 'Get all user' })
   @ApiResponse({ status: 200, type: [User] })
+  @UseGuards(JwtAuthGuard)
   @Get()
   getAll() {
     return this.usersService.getAllUsers()
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common'
+import { Module, forwardRef } from '@nestjs/common'
 import { UsersController } from './users.controller'
 import { UsersService } from './users.service'
 import { SequelizeModule } from '@nestjs/sequelize'
@@ -6,11 +6,16 @@ import { User } from './user.model'
 import { Roles } from 'src/roles/roles.model'
 import { UserRole } from 'src/roles/user-roles.model'
 import { RolesModule } from 'src/roles/roles.module'
+import { AuthModule } from 'src/auth/auth.module'
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [SequelizeModule.forFeature([User, Roles, UserRole]), RolesModule],
+  imports: [
+    SequelizeModule.forFeature([User, Roles, UserRole]),
+    RolesModule,
+    forwardRef(() => AuthModule)
+  ],
   exports: [UsersService]
 })
 export class UsersModule {}
