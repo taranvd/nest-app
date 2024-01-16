cd711ed7ed6ee0e633c2d894b69c9a841bbec3f3
taranvd <apple.vlad.taran@gmail.com>
2024-01-16 19:28:21 +0200

    feat: added roles guars

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,6 +4,8 @@ import { UsersService } from './users.service'
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
 import { User } from './user.model'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { Roles } from 'src/auth/role-auth.decorator'
+import { RolesGuard } from 'src/auth/roles.guard'
 
 @ApiTags('Users')
 @Controller('users')
@@ -19,7 +21,8 @@ export class UsersController {
 
   @ApiOperation({ summary: 'Get all user' })
   @ApiResponse({ status: 200, type: [User] })
-  @UseGuards(JwtAuthGuard)
+  @Roles('ADMIN')
+  @UseGuards(RolesGuard)
   @Get()
   getAll() {
     return this.usersService.getAllUsers()
